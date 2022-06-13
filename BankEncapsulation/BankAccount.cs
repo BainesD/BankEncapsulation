04d46a31b6c01430109532885b0ca5c4a5a9a96c
Baines Deal <bainesdeal@gmail.com>
2022-06-13 14:21:55 -0500

    Add project files.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double _balance = 0;
+        public void Deposit(double deposit)
+        {
+            _balance += deposit;
+        }
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+//In the main method of your application, create a new instance of the BankAccount class. Allow the user of the application to Deposit money and retrieve their balance through the console.
+//Once finished save, commit, and push back to Github
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,50 @@
+﻿    using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var userAccount = new BankAccount();
+            int response;
+            do
+            {
+                Console.WriteLine("Hello! What would you like to do today? \n1 - Deposit\n2 - Get Balance.\n3 - Sign Out");
+                var userInput = int.TryParse(Console.ReadLine(), out response);
+
+                if (!userInput)
+                {
+                    do
+                    {
+                        Console.WriteLine("I'm sorry. That is not a valid response. Please try again.");
+                    }
+                    while (!userInput);
+                }
+                if (response == 1)
+                {
+                    Console.WriteLine("How much will you be depositing today?");
+                    var userDeposit = Double.TryParse(Console.ReadLine(), out double responseDeposit);
+                    if (!userDeposit)
+                    {
+                        do { Console.WriteLine("I'm sorry that is not a valid amount. Please enter a valid amount.");
+                            userDeposit = Double.TryParse(Console.ReadLine(), out responseDeposit);
+                            userAccount.Deposit(responseDeposit);
+                        }
+                        while (!userDeposit);
+                    }
+                    else
+                        userAccount.Deposit(responseDeposit);
+
+                }
+                else if (response == 2)
+                {
+                    Console.WriteLine($"Your account balance is {userAccount.GetBalance()}.");
+                
+                }
+            }
+            while (response != 3);
+            Console.WriteLine("Thank you for your patronage. Have a nice day!");
+        }
+    }
+}
